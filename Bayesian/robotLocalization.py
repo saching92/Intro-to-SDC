dae9feba2b615434e3e25f3fc90b2275e776212e
saching92 <36415849+saching92@users.noreply.github.com>
2018-05-09 22:19:31 -0400

    Update robotLocalization.py

--- Bayesian/robotLocalization.py
+++ Bayesian/robotLocalization.py
@@ -1,12 +1,14 @@
-#Program a function that returns a new distribution 
-#q, shifted to the right by U units. If U=0, q should 
-#be the same as p.
+#Modify the previous code so that the robot senses red twice.
 
-p=[0, 1, 0, 0, 0]
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
-measurements = ['red', 'green']
+measurements = ['red', 'red']
+motions = [1,1]
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q=[]
@@ -19,9 +21,16 @@ def sense(p, Z):
     return q
 
 def move(p, U):
-    #
-    #ADD CODE HERE
-    #
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
     return q
 
-print move(p, 1)
+for k in range(len(measurements)):
+    p = sense(p, measurements[k])
+    p = move(p, motions[k])
+    
+print p         
